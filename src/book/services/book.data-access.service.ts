bc200296ae011b8274e6034a26dcf7975b438996
Edmond <abolfazlmashhadi833@gmail.com>
2025-07-09 13:27:33 +0330

    export data aceess

--- src/book/services/book.data-access.service.ts
+++ src/book/services/book.data-access.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { FindOneOptions, Like, Repository } from 'typeorm';
+import { FindManyOptions, FindOneOptions, Like, Repository } from 'typeorm';
 import { EpisodeEntity } from '../entities/episode.entity';
 import { BookEntity } from '../entities/book.entity';
 import { CategoryEntity } from '../entities/category.entitiy';
+
 
 @Injectable()
 export class BookDataAccess {
@@ -16,7 +17,7 @@ export class BookDataAccess {
 
     @InjectRepository(CategoryEntity)
     private categoryRepository: Repository<CategoryEntity>,
-  ) {}
+  ) { }
 
   findOneById(id: string) {
     return this.bookRepository.findOne({ where: { id } });
@@ -42,5 +43,12 @@ export class BookDataAccess {
 
   findOneCategory(options: FindOneOptions<CategoryEntity>) {
     return this.categoryRepository.findOne(options);
+  }
+
+  listBooks(options: FindManyOptions<BookEntity>) {
+    return this.bookRepository.findAndCount(options)
+  }
+  createQueryBuilder(ailias: string) {
+    return this.bookRepository.createQueryBuilder(ailias)
   }
 }
