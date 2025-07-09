b1981e87ca92d247e0ae6ebace2f91f3a3d4f95f
Edmond <abolfazlmashhadi833@gmail.com>
2025-07-09 17:08:48 +0330

    registred user data access

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,17 +1,18 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
+import { UserService } from './services/user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entity/user.entity';
 import { ListenTimeEntity } from './entity/listenTime.entity';
 import { UserMetaEntity } from './entity/userMeta.entity';
+import { UserDataAcess } from './services/user.dataAcess.service';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([UserEntity, ListenTimeEntity, UserMetaEntity]),
   ],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, UserDataAcess],
   exports: [UserService],
 })
-export class UserModule {}
+export class UserModule { }
