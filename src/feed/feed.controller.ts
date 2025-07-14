814ec68268ce7986e9c7246f69f642daecf80d4f
Edmond <abolfazlmashhadi833@gmail.com>
2025-07-14 16:51:06 +0330

    swagger configured and sample base docs impl need more impl

--- src/feed/feed.controller.ts
+++ src/feed/feed.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param, Query } from '@nestjs/common';
 import { FeedService } from './services/feed.service';
 import { ListRequestDto } from 'src/common/dtos/listRequestDto.dto';
 import { SingleIdValidator } from 'src/common/dtos/single-id-validator';
+import { ApiParam } from '@nestjs/swagger';
 
 @Controller('feed')
 export class FeedController {
@@ -13,7 +14,30 @@ export class FeedController {
   ) {
     return this.feedService.getHomeFeed(query)
   }
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'returns single feed books',
+  })
+  @ApiParam({
+    name: 'take',
+    required: false,
+    description: 'pagination for books in feed',
+    schema: {
+      default: 10,
+      minimum: 1,
+    }
+  })
+  @ApiParam({
+    name: 'skip',
+    required: false,
+    description: 'pagination for books in feed',
 
+    schema: {
+      default: 0,
+      minimum: 0,
+    }
+  })
   @Get('/:id')
   async getSingleFeed(
     @Param() param: SingleIdValidator,
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,10 @@ async function bootstrap() {
     forbidNonWhitelisted: false,
     transform: true,
   }));
+  const swaggerConfig = new DocumentBuilder().setTitle('Satr').setDescription('Swagger documentation for Satr project').setVersion('1.0').build()
+  const docFactory = () => SwaggerModule.createDocument(app, swaggerConfig, { autoTagControllers: true, deepScanRoutes: true }
+  )
+  SwaggerModule.setup('docs', app, docFactory)
 
   await app.listen(process.env.PORT ?? 3000);
 }
